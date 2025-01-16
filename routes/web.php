6dbbbea3d915e33cb1ce1d80894f5c50f6111d4b
fendy <effendyninaas@gmail.com>
2025-01-16 18:58:17 +0700

    add navbar

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
+    return view('home');
+})->name('home');
